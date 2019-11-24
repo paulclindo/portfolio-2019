045714ef39ad1eb2553eb11d5cd4f1604cfeaec6
Paul Ccari <paulclindo@gmail.com>
2019-11-24 14:57:59 -0500

    work on mobile design

--- src/links.js
+++ src/links.js
@@ -1,29 +1,54 @@
-const $exploreBtn = document.getElementById("explore-button");
-const $contactBtn = document.getElementById("contact-button");
+import Flickity from "flickity";
 
-if (window.location.pathname === "/portfolio.html") {
-  const carouselItems = document.getElementsByClassName("carousel-item");
+(function initialFunction() {
+  const $exploreBtn = document.getElementById("explore-button");
+  const $iconSvg = document.getElementsByClassName("Social__networks");
 
-  const linkProjects = [
-    "https://testmoviepc.netlify.com/",
-    "https://github.com/paulclindo/React-PlatziBadges",
-    "https://pauls-spotify.netlify.com/",
-    "https://mymediaplayer.netlify.com/",
-    "https://github.com/paulclindo/CloningPage",
-    "https://paulclindo.github.io/Netflix-Clone/"
+  const socialLinks = [
+    "https://www.linkedin.com/in/paulccari/",
+    "https://github.com/paulclindo",
+    "https://www.instagram.com/paulclindo/",
+    "https://twitter.com/paulclindo"
   ];
 
-  for (let i = 0; i < carouselItems.length; i++) {
-    carouselItems[i].onclick = () => {
-      window.location.replace(linkProjects[i]);
+  for (let i = 0; i < $iconSvg.length; i++) {
+    $iconSvg[i].onclick = () => {
+      console.log(socialLinks[i]);
+      window.location.replace(socialLinks[i]);
     };
   }
-} else if (window.location.pathname === "/home.html") {
-  $exploreBtn.onclick = e => {
-    window.location.replace("portfolio.html");
-  };
-} else {
-  $contactBtn.onclick = e => {
-    window.location.replace("mailto:paulclindo@gmail.com?body=Hey, Paul!");
-  };
-}
+
+  if (window.location.pathname === "/portfolio.html") {
+    const $projectContainer = document.getElementById("favorite-projects");
+    let slider = new Flickity($projectContainer, {
+      wrapAround: false,
+      pageDots: false,
+      prevNextButtons: false,
+      contain: true,
+      groupCells: true,
+      autoPlay: true
+    });
+    const carouselItems = document.getElementsByClassName(
+      "carousel-item__play"
+    );
+
+    const linkProjects = [
+      "https://testmoviepc.netlify.com/",
+      "https://github.com/paulclindo/React-PlatziBadges",
+      "https://pauls-spotify.netlify.com/",
+      "https://mymediaplayer.netlify.com/",
+      "https://github.com/paulclindo/CloningPage",
+      "https://paulclindo.github.io/Netflix-Clone/"
+    ];
+
+    for (let i = 0; i < carouselItems.length; i++) {
+      carouselItems[i].onclick = () => {
+        window.location.replace(linkProjects[i]);
+      };
+    }
+  } else if (window.location.pathname === "/home.html" || "/") {
+    $exploreBtn.onclick = e => {
+      window.location.replace("portfolio.html");
+    };
+  }
+})();
