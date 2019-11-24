c8ce2953ce89ba72ca0494c1f54c616c8cbcb0d8
Paul Ccari <paulclindo@gmail.com>
2019-11-23 21:17:32 -0500

    pure html css js 0.1

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
 import "normalize.css";
+import "./links";
 // import "./assets/styles/components/components.scss";
 // import "./assets/styles/global.scss";
---
+++ src/links.js
@@ -0,0 +1,32 @@
+const $exploreBtn = document.getElementById("explore-button");
+const $contactBtn = document.getElementById("contact-button");
+
+if (window.location.pathname === "/portfolio.html") {
+  const carouselItems = document.getElementsByClassName("carousel-item");
+
+  const linkProjects = [
+    "https://testmoviepc.netlify.com/",
+    "https://github.com/paulclindo/React-PlatziBadges",
+    "https://pauls-spotify.netlify.com/",
+    "https://mymediaplayer.netlify.com/",
+    "https://github.com/paulclindo/CloningPage",
+    "https://paulclindo.github.io/Netflix-Clone/"
+  ];
+
+  for (let i = 0; i < carouselItems.length; i++) {
+    carouselItems[i].onclick = () => {
+      window.location.replace(linkProjects[i]);
+    };
+  }
+}
+
+if (window.location.pathname === "/home.html") {
+  $exploreBtn.onclick = e => {
+    window.location.replace("portfolio.html");
+  };
+}
+if (window.location.pathname === "/contact.html") {
+  $contactBtn.onclick = e => {
+    window.location.replace("mailto:paulclindo@gmail.com?body=Hey, Paul!");
+  };
+}
