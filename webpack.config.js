b7aca366d91b55d097982fbc73c0aae47e011ad8
Paul Ccari <paulclindo@gmail.com>
2020-05-25 02:26:11 -0500

    fixed webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,10 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const TersetJSPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-  entry: {
-    app: path.resolve(__dirname, 'src/index.js'),
-  },
+  entry: './src/index.js'
+  // {
+  //   app: path.resolve(__dirname, 'src/index.js'),
+  // },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'js/[name].[hash].js',
